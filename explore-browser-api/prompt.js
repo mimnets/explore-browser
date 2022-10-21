0de9d633e3a4de83bf30c47363991e2b5cafe4df
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-10-21 21:47:16 +0600

    first commit

---
+++ explore-browser-api/prompt.js
@@ -0,0 +1,16 @@
+const showAlert = () =>{
+    const num = Math.random()*10;
+    if(num < 5){
+        alert(num + ' is less then 5')
+    }
+}
+
+const askSomething = () =>{
+    const decision = confirm('Are you coming to office?')
+    if(decision === true){
+        alert('Please come on time');
+    }
+    else{
+        alert('You are dismiss!')
+    }
+}
